030b62c60f847ebf90d89257ddddd7db5f5681fb
bongofury <jtroutmanman@gmail.com>
2021-06-12 20:44:24 -0500

    created app/services directory in backend and created a list serializer to generate json for fetching lists. created #index method in lists controller and added code to gather lists and render json.

--- top-five-backend/app/controllers/lists_controller.rb
+++ top-five-backend/app/controllers/lists_controller.rb
@@ -1,2 +1,9 @@
 class ListsController < ApplicationController
+    def index
+        if params[:category_id]
+            category = Category.find_by_id(params[:category_id])
+            lists = category.lists
+            render json: ListSerializer.new(lists).to_serialized_json
+        end
+    end
 end
---
+++ top-five-backend/app/services/list_serializer.rb
@@ -0,0 +1,11 @@
+class ListSerializer
+    def initialize(lists_object)
+        @lists = lists_object
+    end
+
+    def to_serialized_json
+        @lists.to_json(include: {
+            items_ranked: {only: [:name, :rank]}
+        }, except: [:created_at, :updated_at])
+    end
+end

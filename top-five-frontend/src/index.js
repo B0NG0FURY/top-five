726bfa966aaea13b41b06989781c29c6e1735541
bongofury <jtroutmanman@gmail.com>
2021-06-12 19:08:58 -0500

    added functions in frontend index.js to fetch categories and display them on page load.

--- top-five-frontend/src/index.js
+++ top-five-frontend/src/index.js
@@ -8,10 +8,26 @@ class Category {
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-
+document.addEventListener("DOMContentLoaded", () => { 
+    getCategories().then(categories => {
+        let div = document.querySelector("div.category-select");
+        let select = document.createElement("select");
+        select.setAttribute("name", "categories");
+        categories.forEach(category => {
+            addCategory(category, select);
+        });
+        div.appendChild(select);
+    });
 })
 
 function getCategories() {
     return fetch(CATEGORIES_URL).then(resp => resp.json());
+}
+
+function addCategory(category, element) {
+    let cat = new Category(category["id"], category["name"]);
+    let option = document.createElement("option");
+    option.value = `${cat.name}`;
+    option.text = `${cat.name}`;
+    element.appendChild(option);
 }

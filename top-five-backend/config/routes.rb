229105fa157862cd324b90483ab6ea97be52625e
bongofury <jtroutmanman@gmail.com>
2021-06-12 20:08:05 -0500

    added nested route in backend for categories/:category_id/lists in routes.rb.

--- top-five-backend/config/routes.rb
+++ top-five-backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :items
   resources :lists
-  resources :categories, only: [:index, :create]
+  resources :categories, only: [:index, :create] do
+    resources :lists, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

fbf76394dd9a3c7588d89fd89e51c6810dc5507f
bongofury <jtroutmanman@gmail.com>
2021-06-19 21:36:27 -0500

    added nested attributes for items paremeter to list model in backend.

--- top-five-backend/app/models/list.rb
+++ top-five-backend/app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
     belongs_to :category
     has_many :items
+    accepts_nested_attributes_for :lists
 
     def items_ranked
         self.items.sort_by {|item| item.rank}

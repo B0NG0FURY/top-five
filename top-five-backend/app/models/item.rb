0cf867643a75650f56f4ef05c1c7b4256d079564
bongofury <jtroutmanman@gmail.com>
2021-07-05 12:03:18 -0500

    added validations and normalizations on backend to lists and items. lists are now titleized before they are saved. items are checked to make sure ranks are within a 1 to 5 range. item names are titleized before they are saved.

--- top-five-backend/app/models/item.rb
+++ top-five-backend/app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
     belongs_to :list
+    validates :rank, :inclusion => 1..5
+    before_save :normalize_name
+
+    private
+
+    def normalize_name
+        self.name = self.name.titleize
+    end
 end
--- top-five-backend/app/models/list.rb
+++ top-five-backend/app/models/list.rb
@@ -2,8 +2,15 @@ class List < ApplicationRecord
     belongs_to :category
     has_many :items
     accepts_nested_attributes_for :items
+    before_save :normalize_title
 
     def items_ranked
         self.items.sort_by {|item| item.rank}
+    end
+
+    private
+
+    def normalize_title
+        self.title = self.title.titleize
     end
 end
